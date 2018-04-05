c68b6e9193818c62b5e808b9d3ca2e5d2fd8e485
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-04-06 02:29:58 +0700

    setup redux and tested with simple component

--- aJokeFromChuckNorris/client/src/App.js
+++ aJokeFromChuckNorris/client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-
+import { connect } from "react-redux";
+import FetchingButton from "./components/fetchingbutton";
+import { lifecycle, compose } from "recompose";
+import { fetchAllJokes, fetchJokeById } from "./actions";
 const Wrapper = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content:flex-start;
-  align-items:center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
   max-width: 360px;
   min-height: 480px;
   padding: 2em;
@@ -13,12 +16,50 @@ const Wrapper = styled.div`
   margin-top: 2em;
   border-radius: 12px;
   box-shadow: 1px 2px 4px black;
-  
 `;
+
+const componentName = props => {
+  console.log(props.jokes);
+
+  return (
+    <div>
+      {props.jokes.map(({ id, joke }) => (
+        <div key={id}>
+          <h2>{id}</h2>
+          <p>{joke}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const enhance = compose(
+  connect(
+    state => ({
+      jokes: state.jokes
+    }),
+    { fetchAllJokes, fetchJokeById }
+  ),
+  lifecycle({
+    componentDidMount() {
+      console.log("recompose");
+
+      this.props.fetchJokeById(1);
+    }
+  })
+);
+
+const A = enhance(componentName);
 
 class App extends Component {
   render() {
-    return <Wrapper><h1>Chuck Norris</h1></Wrapper>;
+    return (
+      <Wrapper>
+        <h1>Chuck Norris</h1>
+        <A />
+        <FetchingButton />
+      </Wrapper>
+    );
   }
 }
 
--- aJokeFromChuckNorris/client/src/index.js
+++ aJokeFromChuckNorris/client/src/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer,applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
