cb38ff4491707b493a4bb9e4fd998c48d40b79c1
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-04-06 00:05:42 +0700

    added Wrapper

--- aJokeFromChuckNorris/client/src/App.js
+++ aJokeFromChuckNorris/client/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:flex-start;
+  align-items:center;
+  max-width: 360px;
+  min-height: 480px;
+  padding: 2em;
+  margin: auto;
+  margin-top: 2em;
+  border-radius: 12px;
+  box-shadow: 1px 2px 4px black;
+  
+`;
 
 class App extends Component {
   render() {
-    return <div />;
+    return <Wrapper><h1>Chuck Norris</h1></Wrapper>;
   }
 }
 
