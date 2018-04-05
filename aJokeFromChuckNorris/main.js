ec4ba26f9bb43e33f9752724b778d77b084cb9b2
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-04-05 23:07:50 +0700

    added sample randome joke route

--- aJokeFromChuckNorris/main.js
+++ aJokeFromChuckNorris/main.js
@@ -1,12 +1,21 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
-
+const jokesAPI = require("./jokesAPI");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.get("/random/", async (req, res) => {
+  try {
+    const randomedData = await jokesAPI.getRandomJoke();
+    return res.json(randomedData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 app.listen(3000, () => {
