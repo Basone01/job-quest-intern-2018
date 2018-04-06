e4984b024e18e6d0e2f5dd73ad1235e732d12564
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-04-06 14:43:25 +0700

    moved wrapper to components folder, removed test component

--- aJokeFromChuckNorris/client/src/App.js
+++ aJokeFromChuckNorris/client/src/App.js
@@ -4,59 +4,15 @@ import { connect } from "react-redux";
 import FetchingButton from "./components/fetchingbutton";
 import { lifecycle, compose } from "recompose";
 import { fetchAllJokes, fetchJokeById } from "./actions";
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  max-width: 360px;
-  min-height: 480px;
-  padding: 2em;
-  margin: auto;
-  margin-top: 2em;
-  border-radius: 12px;
-  box-shadow: 1px 2px 4px black;
-`;
-
-const componentName = props => {
-  console.log(props.jokes);
-
-  return (
-    <div>
-      {props.jokes.map(({ id, joke }) => (
-        <div key={id}>
-          <h2>{id}</h2>
-          <p>{joke}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const enhance = compose(
-  connect(
-    state => ({
-      jokes: state.jokes.jokes
-    }),
-    { fetchAllJokes, fetchJokeById }
-  ),
-  lifecycle({
-    componentDidMount() {
-      console.log("recompose");
-
-      this.props.fetchJokeById(102);
-    }
-  })
-);
-
-const A = enhance(componentName);
+import { Wrapper } from "./components/styled";
+import JokeList from "./components/jokeList";
 
 class App extends Component {
   render() {
     return (
       <Wrapper>
         <h1>Chuck Norris</h1>
-        <A />
+        <JokeList />
         <FetchingButton />
       </Wrapper>
     );
---
+++ aJokeFromChuckNorris/client/src/components/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  max-width: 360px;
+  min-height: 480px;
+  padding: 2em;
+  margin: auto;
+  margin-top: 2em;
+  border-radius: 12px;
+  box-shadow: 1px 2px 4px black;
+`;
