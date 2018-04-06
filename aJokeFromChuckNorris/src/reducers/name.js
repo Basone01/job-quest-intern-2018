10c93188bcd7d6dc92efc66f39bf2c1e5badd24f
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-04-07 03:09:38 +0700

    added default name in name reducer

--- aJokeFromChuckNorris/src/reducers/name.js
+++ aJokeFromChuckNorris/src/reducers/name.js
@@ -1,4 +1,7 @@
-import { CHANGE_CHARACTER_NAME, RESET_CHARACTER_NAME } from "../constants/actionTypes";
+import {
+  CHANGE_CHARACTER_NAME,
+  RESET_CHARACTER_NAME
+} from "../constants/actionTypes";
 const initialState = {
   firstName: "Chuck",
   lastName: "Norris"
@@ -7,8 +10,17 @@ const initialState = {
 export default function jokesReducer(state = initialState, action) {
   switch (action.type) {
     case CHANGE_CHARACTER_NAME:
-      return (!action.payload)?state:action.payload;
-      
+      if (!action.payload) return state;
+      else {
+        let { firstName, lastName } = action.payload;
+        if (firstName === "" && lastName === "") {
+          firstName = "Chuck";
+          lastName = "Norris";
+        }
+
+        return { firstName, lastName };
+      }
+
     case RESET_CHARACTER_NAME:
       return {
         firstName: "Chuck",
