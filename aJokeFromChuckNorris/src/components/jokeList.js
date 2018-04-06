e397008b3efbabaa04f0f9ce11c20557e6ea9f5e
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-04-07 06:57:15 +0700

    fix Quote with dangerouslyHtml

--- aJokeFromChuckNorris/src/components/jokeList.js
+++ aJokeFromChuckNorris/src/components/jokeList.js
@@ -2,13 +2,16 @@ import React from "react";
 import { compose, mapProps, lifecycle, withProps } from "recompose";
 import { connect } from "react-redux";
 import { fetchJokeById } from "../actions/";
+import { Quote, Flex } from "./styled";
 
 const JokeItem = props => {
   const { id, joke, catagories } = props;
   return (
     <div>
       <h2>{id}</h2>
-      <p>{joke}</p>
+      <Quote dangerouslySetInnerHTML={{__html: joke}}></Quote>
+      
+      <hr />
     </div>
   );
 };
@@ -19,7 +22,7 @@ const enhance = compose(
 
 const JokeList = props => {
   return (
-    <div>{props.jokes.map(joke => <JokeItem key={joke.id} {...joke} />)}</div>
+    <Flex fd="column">{props.jokes.map(joke => <JokeItem key={joke.id} {...joke} />)}</Flex>
   );
 };
 
